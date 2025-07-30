e8b1ded2cfddf690425a11ef20397c8358738b0d
Nicolas Reymundo <nreymundo@users.noreply.github.com>
2025-07-30 02:19:21 +0200

    Remove unused checks from hugepages cleanup script

--- etc/libvirt/hooks/qemu.d/win11/release/end/50-hugepages.sh
+++ etc/libvirt/hooks/qemu.d/win11/release/end/50-hugepages.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Target domain name (optional, if you want to match)
-VM_NAME="$1"
-ACTION="$2"
-
-# Safety check
-if [[ "$VM_NAME" != "win11" ]]; then
-  echo "[INFO] Skipping hugepage cleanup for unrelated VM: $VM_NAME"
-  exit 0
-fi
-
 # Free hugepages
 echo 0 | sudo tee /proc/sys/vm/nr_hugepages > /dev/null
 echo "[INFO] Hugepages released."
